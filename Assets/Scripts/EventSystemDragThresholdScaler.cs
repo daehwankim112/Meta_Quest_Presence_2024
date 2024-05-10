ad10815645125fe9ad93bb02b7b9c32a6a8f919b
NuiN99 <endyverse@gmail.com>
2024-05-09 23:18:29 -0700

    fix: inputsystem dragthreshold too small for giant

---
+++ Assets/Scripts/EventSystemDragThresholdScaler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystemDragThresholdScaler : MonoBehaviour
+{
+    [SerializeField] EventSystem eventSystem;
+
+    int _initialDragThreshold;
+    
+    void Awake()
+    {
+        _initialDragThreshold = eventSystem.pixelDragThreshold;
+    }
+
+    void OnEnable()
+    {
+        GameEvents.OnLobbyHosted += ScaleUp;
+        GameEvents.OnRoomScaled += ScaleDown;
+    }
+
+    void OnDisable()
+    {
+        GameEvents.OnLobbyHosted -= ScaleUp;
+        GameEvents.OnRoomScaled -= ScaleDown;
+    }
+    
+    void ScaleDown()
+    {
+        eventSystem.pixelDragThreshold = _initialDragThreshold;
+    }
+
+    void ScaleUp()
+    {
+        eventSystem.pixelDragThreshold = _initialDragThreshold * Mathf.RoundToInt(RoomEnvironmentInitializer.RoomScale.magnitude);
+    }
+}
