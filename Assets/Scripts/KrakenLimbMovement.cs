54b9367fa941ad9b1e4258a0355c148c51eb2379
NuiN99 <endyverse@gmail.com>
2024-05-13 09:59:17 -0700

    feat: kraken random limb movement

--- Assets/Scripts/KrakenLimbMovement.cs
+++ Assets/Scripts/KrakenLimbMovement.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 
 public class KrakenLimbMovement : MonoBehaviour
 {
-    [SerializeField] Rigidbody rb;
     [SerializeField] RandomTimer forceInterval;
     
     [SerializeField] float minForce, maxForce;
+    
+    Rigidbody _rb;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
         if (!forceInterval.Complete()) return;
 
         float force = Random.Range(minForce, maxForce);
-        rb.AddForce(Random.insideUnitSphere.normalized * force, ForceMode.VelocityChange);
+        _rb.AddForce(Random.insideUnitSphere.normalized * force, ForceMode.VelocityChange);
     }
 }
